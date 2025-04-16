d6c0fd6b086d8d10d35f43bb3e5d15bb86793e55
Sahim Salem <sahim.salem@brainstation-23.com>
2025-04-16 17:12:24 +0600

    Design home screen UI and configure routes for Add and Edit pages

---
+++ lib/view/edit_task_screen.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_task_manager/routing/app_route_name.dart';
+import 'package:go_router/go_router.dart';
+
+class EditTaskScreen extends StatelessWidget {
+  const EditTaskScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Edit task'),
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back),
+          onPressed: () {
+            if (context.canPop()) {
+              context.pop();
+            } else {
+              context.go(RouteNames.taskListScreen);
+            }
+          },
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Center(child: Text('Edit task page')),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          context.go(RouteNames.editTaskSceen);
+        },
+        child: Icon(Icons.edit),
+      ),
+    );
+  }
+}
--- lib/view/task_list_screen.dart
+++ lib/view/task_list_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_task_manager/routing/app_route_name.dart';
+import 'package:go_router/go_router.dart';
 
 class TaskListScreen extends StatelessWidget {
   const TaskListScreen({super.key});
@@ -7,11 +9,66 @@ class TaskListScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("My Tasks"),
         centerTitle: true,
-        backgroundColor: Color(0xFFBBDEFB),
+        title: Text(
+          'Task List',
+          style: Theme.of(context).textTheme.titleLarge?.copyWith(
+            color: Theme.of(context).appBarTheme.foregroundColor,
+          ),
+        ),
       ),
-      body: Center(child: Text('Home page')),
+      body: ListView.builder(
+        itemCount: 6,
+        itemBuilder: (context, index) {
+          return Card(
+            color:
+                index % 2 == 0
+                    ? Theme.of(context).scaffoldBackgroundColor
+                    : Theme.of(context).cardColor,
+            margin: EdgeInsets.symmetric(vertical: 4, horizontal: 8),
+            child: ListTile(
+              title: Text(
+                'Todo task $index',
+                style: Theme.of(context).textTheme.titleLarge?.copyWith(
+                  decoration:
+                      index % 2 == 0 ? TextDecoration.lineThrough : null,
+                  color:
+                      index % 2 == 0
+                          ? Theme.of(context).primaryColor
+                          : Theme.of(context).textTheme.titleLarge?.color,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+              subtitle: Text(
+                'Todo task description for task $index',
+                style: Theme.of(context).textTheme.bodyMedium?.copyWith(
+                  decoration:
+                      index % 2 == 0 ? TextDecoration.lineThrough : null,
+                  color: Theme.of(context).textTheme.bodyMedium?.color,
+                ),
+              ),
+              leading: Checkbox(
+                value: index % 2 == 0,
+                onChanged: (value) {},
+                activeColor: Theme.of(context).primaryColor,
+              ),
+              onTap: () {
+                context.go(RouteNames.editTaskSceen);
+              },
+            ),
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          context.go(RouteNames.addTaskScreen);
+        },
+        backgroundColor: Theme.of(context).primaryColor,
+        child: Icon(
+          Icons.add_task_sharp,
+          color: Theme.of(context).iconTheme.color,
+        ),
+      ),
     );
   }
 }
