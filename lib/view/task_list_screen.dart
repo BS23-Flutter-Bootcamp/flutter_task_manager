4f9cf2debe93abdc295510ac8e4384b2ddb84a18
Sahim Salem <sahim.salem@brainstation-23.com>
2025-04-17 15:03:57 +0600

    Update task screen list  UI with title, due date, and task notes field

--- lib/view/task_list_screen.dart
+++ lib/view/task_list_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_task_manager/routes/app_route_name.dart';
 import 'package:go_router/go_router.dart';
+import 'package:intl/intl.dart';
 
 class TaskListScreen extends StatelessWidget {
   const TaskListScreen({super.key});
@@ -25,36 +26,58 @@ class TaskListScreen extends StatelessWidget {
                 index % 2 == 0
                     ? Theme.of(context).scaffoldBackgroundColor
                     : Theme.of(context).cardColor,
-            margin: EdgeInsets.symmetric(vertical: 4, horizontal: 8),
+            margin: const EdgeInsets.symmetric(vertical: 6, horizontal: 12),
+            elevation: 2,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(14),
+            ),
             child: ListTile(
-              title: Text(
-                'Todo task $index',
-                style: Theme.of(context).textTheme.titleLarge?.copyWith(
-                  decoration:
-                      index % 2 == 0 ? TextDecoration.lineThrough : null,
-                  color:
-                      index % 2 == 0
-                          ? Theme.of(context).primaryColor
-                          : Theme.of(context).textTheme.titleLarge?.color,
-                  fontWeight: FontWeight.w500,
-                ),
+              contentPadding: const EdgeInsets.symmetric(
+                vertical: 10,
+                horizontal: 16,
               ),
-              subtitle: Text(
-                'Todo task description for task $index',
-                style: Theme.of(context).textTheme.bodyMedium?.copyWith(
-                  decoration:
-                      index % 2 == 0 ? TextDecoration.lineThrough : null,
-                  color: Theme.of(context).textTheme.bodyMedium?.color,
-                ),
-              ),
+              onTap: () {
+                context.go(RouteNames.editTaskSceen);
+              },
               leading: Checkbox(
                 value: index % 2 == 0,
                 onChanged: (value) {},
                 activeColor: Theme.of(context).primaryColor,
               ),
-              onTap: () {
-                context.go(RouteNames.editTaskSceen);
-              },
+              title: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Todo task $index',
+                    style: Theme.of(context).textTheme.titleMedium?.copyWith(
+                      decoration:
+                          index % 2 == 0 ? TextDecoration.lineThrough : null,
+                      color:
+                          index % 2 == 0
+                              ? Theme.of(context).primaryColor
+                              : Theme.of(context).textTheme.titleMedium?.color,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                  const SizedBox(height: 4),
+                  Text(
+                    'Todo task description for task $index',
+                    style: Theme.of(context).textTheme.bodyMedium?.copyWith(
+                      decoration:
+                          index % 2 == 0 ? TextDecoration.lineThrough : null,
+                      color: Theme.of(context).textTheme.bodyMedium?.color,
+                    ),
+                  ),
+                  const SizedBox(height: 4),
+                  Text(
+                    'Due Date: ${DateFormat.yMMMd().format(DateTime.now())}',
+                    style: Theme.of(context).textTheme.bodySmall?.copyWith(
+                      color: Theme.of(context).hintColor,
+                      fontStyle: FontStyle.italic,
+                    ),
+                  ),
+                ],
+              ),
             ),
           );
         },
@@ -64,10 +87,7 @@ class TaskListScreen extends StatelessWidget {
           context.go(RouteNames.addTaskScreen);
         },
         backgroundColor: Theme.of(context).primaryColor,
-        child: Icon(
-          Icons.add,
-          color: Theme.of(context).iconTheme.color,
-        ),
+        child: Icon(Icons.add, color: Theme.of(context).iconTheme.color),
       ),
     );
   }
