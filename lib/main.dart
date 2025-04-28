abbca56641b1cb6140dcf1ed71ce0491181cd0df
Sahim Salem <sahim.salem@brainstation-23.com>
2025-04-28 19:39:48 +0600

    Clean up main.dart file

--- lib/main.dart
+++ lib/main.dart
@@ -10,57 +10,54 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_task_manager/viewmodel/login_screen_view_model.dart';
 import 'package:provider/provider.dart';
 
-
 Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await _initializeApp();
+  runApp(const MyApp());
+}
+
+Future<void> _initializeApp() async {
   try {
-    WidgetsFlutterBinding.ensureInitialized();
     await dotenv.load();
     await Firebase.initializeApp(
       options: DefaultFirebaseOptions.currentPlatform,
     );
-
-    // Initialize the notification service
-    NotificationService notificationService = NotificationService();
-    await notificationService.init();
-
-    // Check remember me status
-    final loginService = LoginService();
-    await loginService.init();
-    await loginService.checkRememberMeStatus();
-  } catch (e) {
+    await NotificationService().init();
+    LoginService()
+      ..init()
+      ..checkRememberMeStatus();
+  } catch (e, stackTrace) {
     if (kDebugMode) {
-      print('Initialization failed: $e');
+      debugPrint('Initialization failed: $e\n$stackTrace');
     }
   }
-
-  runApp(
-    MultiProvider(
-      providers: [
-        ChangeNotifierProvider(create: (_) => LoginViewModel()),
-      ],
-      child: const MainApp(),
-    ),
-  );
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      debugShowCheckedModeBanner: false,
-      routerConfig: router,
-      theme: ThemeData(
-        primaryColor: AppConstants.primaryColor,
-        hintColor: AppConstants.hintColor,
-        scaffoldBackgroundColor: AppConstants.scaffoldBackgroundColor,
-        textTheme: TextTheme(
-          bodyLarge: TextStyle(color: AppConstants.textColorDark),
-          bodyMedium: TextStyle(color: AppConstants.textColorLight),
-        ),
-        appBarTheme: AppBarTheme(color: AppConstants.textColorDark),
+    return MultiProvider(
+      providers: [ChangeNotifierProvider(create: (_) => LoginViewModel())],
+      child: MaterialApp.router(
+        debugShowCheckedModeBanner: false,
+        routerConfig: router,
+        theme: _buildThemeData(),
       ),
     );
   }
+}
+
+ThemeData _buildThemeData() {
+  return ThemeData(
+    primaryColor: AppConstants.primaryColor,
+    hintColor: AppConstants.hintColor,
+    scaffoldBackgroundColor: AppConstants.scaffoldBackgroundColor,
+    textTheme: TextTheme(
+      bodyLarge: TextStyle(color: AppConstants.textColorDark),
+      bodyMedium: TextStyle(color: AppConstants.textColorLight),
+    ),
+    appBarTheme: AppBarTheme(color: AppConstants.textColorDark),
+  );
 }
